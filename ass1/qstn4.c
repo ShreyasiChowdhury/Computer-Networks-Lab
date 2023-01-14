1d06d01757cfb54585360fba4010b5b7b85e812a
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2023-01-14 20:59:36 +0530

    class assignment

--- ass1/qstn4.c
+++ ass1/qstn4.c
@@ -1,17 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+#define size 20
+int checkFreq(char arr[],int n)
+{
+	int i,count=0;
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]=='1')
+		{
+			count++;
+		}
+	}
+	return count;
+}
 int main()
 {
-	char str[20];
-	int seg;
-	printf("Enter word: ");
+	char str[size];
+	printf("Input: ");
 	gets(str);
-	printf("Enter segment length: ");
-	scanf("%d",&seg);
 	int n=strlen(str);
-	char arr[20]={'\0'};
-	int i=0,j=0;
-	while(n>seg)
+	printf("Segment Length: ");
+	int seg;
+	scanf("%d",&seg);
+	char arr[size]={'\0'};
+	int i=0,j=0,x=0,y=0;
+	while(n!=seg)
 	{
 		arr[i]='0';
 		i++;
@@ -20,6 +33,26 @@ int main()
 			break;
 	}
 	strcat(arr,str);
-	printf("\n\n%s\n",arr);
+	printf("\nOutput:\n");
+	int segno=n/seg;
+	char segarr[seg];
+	int freq[segno];
+	for(i=0;i<n;i=i+seg)
+	{
+		printf("Segment %d: ",y+1);
+		for(j=i;j<seg+i;j++,x++)
+		{
+			segarr[x]=arr[j];
+		}
+		x=0;
+		printf("%s\n",segarr);
+		freq[y]=checkFreq(segarr,seg);
+		y++;
+	}
+	printf("\nFrequency:\n");
+	for(i=0;i<segno;i++)
+	{
+		printf("Segment %d: %d\n",i+1,freq[i]);
+	}
 	return 0;
 }
